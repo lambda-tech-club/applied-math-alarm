d2874c968ba6523811f186354a8d30838a6372ce
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-12 03:34:11 +0000

    要件に合うように修正

--- pages/quadratic-equation.tsx
+++ pages/quadratic-equation.tsx
@@ -13,6 +13,7 @@ import styles from '../styles/Home.module.css'
 
 interface Question {
   x: number | null
+  x2: number | null
   a: number
   b: number
   c: number
@@ -23,6 +24,7 @@ const randint = (min: number, max: number): number => Math.floor(Math.random() *
 const QEquation: NextPage = ({ chime, correct, incorrect }: any) => {
   const createQuestion = (): Question => {
     let x: number | null = null
+    let x2: number | null = null
     let a: number
     let b: number
     let c: number
@@ -34,27 +36,42 @@ const QEquation: NextPage = ({ chime, correct, incorrect }: any) => {
       b = randint(2, 12) // coefficient of x
       c = a * x * x + b * x
       dis = b * b - 4 * a * c
-    } while (dis < 0 || Math.sqrt(dis) % 1 !== 0) // √b^2 - 4acが整数でない場合や、判別式が負の場合に再抽選
+    } while (dis < 0 || Math.sqrt(dis) % 1 !== 0 || (x == 0 && Math.random() > 0.3)) // √b^2 - 4acが整数でない場合や、判別式が負の場合に再抽選&調整
+
+    // x2を計算 (二つ目の解)
+    x2 = (-b - Math.sqrt(dis)) / (2 * a)
 
     return {
       x,
+      x2,
       a,
       b,
       c,
     }
-  }
+}
+
 
   const [questionText, setQuestionText] = useState("")
   const [answers, setAnswers] = useState<(number | null)[]>([])
-  useEffect(() => {
+  useEffect((): any => {
     const questionData = createQuestion()
     const mathJax = String.raw`${questionData.a}x^2 + ${questionData.b}x = ${questionData.c}` // 問題文生成
     setQuestionText(mathJax)
-    setAnswers([questionData.x])
+    let answerBox = [questionData.x, questionData.x2]
+    if (questionData.x == null || questionData.x2 == null) return [0,0];
+    if (questionData.x2 > questionData.x) {
+      answerBox = [questionData.x2 , questionData.x]
+    }
+    setAnswers(answerBox)
   }, [])
 
   const verify = async () => {
-    const parsedAnswers = ans.split(",").map(answer => answer.trim())
+    let myAnswerBox = [ans, ans2]
+    if (ans2 > ans) {
+      myAnswerBox = [ans2, ans]
+    }
+    const ansBox = `${myAnswerBox[0]},${myAnswerBox[1]}`;
+    const parsedAnswers = ansBox.split(",").map(answer => answer.trim())
 
     const parsedIntAnswers = parsedAnswers.map(answer => {
       const parsedFraction = answer.split("/") // 分数処理
@@ -75,10 +92,11 @@ const QEquation: NextPage = ({ chime, correct, incorrect }: any) => {
 
     const filteredAnswers = parsedIntAnswers.filter(answer => answer !== null) as number[]
 
-    if (answers.some(answer => filteredAnswers.includes(answer))) {
+    console.log(filteredAnswers, answers)
+    if (Math.floor(answers[0]!) == Math.floor(filteredAnswers[0]) && Math.floor(answers[1]!) == Math.floor(filteredAnswers[1])) {
       chime.current.pause()
       correct.current.play()
-      const result = await fetch(`/api/send?command=1`) // Faild to Fetch ..?
+      const result = await fetch(`/api/send?command=1`) 
       console.log(await result.json())
       router.push('/wakeup')
     } else {
@@ -87,13 +105,32 @@ const QEquation: NextPage = ({ chime, correct, incorrect }: any) => {
       setTimeout(() => {
         setflash(false)
         setAns('')
+        setAns2('')
       }, 200)
     }
   }
 
   const router = useRouter()
-  const [ans, setAns] = useState('')
+  const [ans, setAns] = useState("")
+  const [ans2, setAns2] = useState("")
   const [isFlash, setflash] = useState(false)
+
+  function addMinus(target: number) {
+    if (target == 1) {
+      setAns(ans + "-")
+    }else {
+      setAns2(ans2 + "-")
+    }
+  }
+
+  function addWeri(target: number) {
+    if (target == 1) {
+      setAns(ans + "/")
+    }else {
+      setAns2(ans2 + "/")
+    }
+  }
+
 
   return (
     <>
@@ -104,8 +141,21 @@ const QEquation: NextPage = ({ chime, correct, incorrect }: any) => {
           <MathJax.Node formula={questionText} />
           解を求めよ 
         </MathJax.Provider>
-        <div>
-          <input className={styles.textbox} placeholder="x1 , x2" type="text" value={ans} onChange={(e) => { setAns(e.target.value) }} inputMode="numeric"/>
+        <div className={styles.textbox_box}>
+          <MathJax.Provider>
+            <MathJax.Node formula="x =" />
+            <div className={styles.textbox_method}>
+              <button onClick={() => {addMinus(1)}}>ー</button>
+              <button onClick={() => {addWeri(1)}}>/</button>
+            </div>
+            <input className={styles.textbox_child} placeholder="x1" type="text" value={ans} onChange={(e) => { setAns(e.target.value) }} inputMode="numeric"/>
+            <span className={styles.textbox_comma}>,</span>
+            <div className={styles.textbox_method}>
+              <button onClick={() => {addMinus(2)}}>ー</button>
+              <button onClick={() => {addWeri(2)}}>/</button>
+            </div>
+            <input className={styles.textbox_child} placeholder="x2" type="text" value={ans2} onChange={(e) => { setAns2(e.target.value) }} inputMode="numeric"/>
+          </MathJax.Provider>
         </div>
         <button className={styles.button} onClick={verify}>
           決定
